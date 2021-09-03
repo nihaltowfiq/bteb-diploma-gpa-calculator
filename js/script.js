de33b49e6ea066ff2e0b3667846011e0abd0a152
nihaltowfiq <nihaltowfiq@gmail.com>
2021-09-04 01:12:35 +0600

    final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const cgpaForm = document.getElementById('cgpa_form');
 const probidhan = document.getElementById('probidhan');
 const cgpaInputs = document.querySelectorAll('.form-control');
-const formSubmitBtn = document.getElementById('#form_submit');
+const resultArea = document.querySelector('.result_group');
+const extarActionArea = document.querySelector('.extra_action');
+const submitBtn = document.getElementById('#form_submit');
+const printBtn = document.querySelector('.print');
+const resetBtn = document.querySelector('.reset');
 
 let allSemesterGPA = [];
 const importanceOfGPA_2010 = [5, 5, 5, 15, 15, 20, 25, 10];
@@ -19,6 +23,7 @@ cgpaInputs.forEach((input, index) => {
                 element.style.border = '1px solid var(--light) ';
             }
         } else {
+            delete allSemesterGPA[index];
             if (!document.querySelector(`.m${index}`)) {
                 const message = document.createElement('p');
                 message.classList = `text-danger invalid_message m${index}`;
@@ -30,30 +35,39 @@ cgpaInputs.forEach((input, index) => {
                 element.style.border = '2px solid var(--danger)';
             }
         }
+        console.log({ allSemesterGPA });
     });
 });
 
 cgpaForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let isArrValid = null;
+    const invalid = allSemesterGPA.findIndex((el) => el === undefined);
+
     if (allSemesterGPA.length === 8) {
-        allSemesterGPA.find((el) => {
-            if (el === undefined) {
-                isArrValid = true;
-            } else {
-                isArrValid = false;
-            }
-        });
+        if (invalid === -1) {
+            const cgpa = getResult(allSemesterGPA);
+            const finalResult = (Math.round(cgpa * 100) / 100).toFixed(2);
+            resultArea.innerHTML = `
+                <h5 class="text-warning">Your cGPA is ${finalResult}</h5>
+            `;
+            extarActionArea.style.display = 'block';
+        } else if (invalid !== -1) {
+            showValidateMsg();
+        }
+    } else {
+        showValidateMsg();
     }
+});
 
-    console.log(isArrValid);
-
-    // console.log('specialCase', typeof allSemesterGPA[0]);
+printBtn.addEventListener('click', () => {
+    window.print();
+});
+resetBtn.addEventListener('click', () => {
+    window.location.reload();
 });
 
 const getResult = (gpa) => {
     const selectedProbidhan = probidhan.value;
-
     let cGpa = 0;
     if (selectedProbidhan === '2010') {
         cGpa = getCgpa(gpa, importanceOfGPA_2010);
@@ -68,7 +82,20 @@ const getCgpa = (gpa, formula) => {
         .map((gpa) => gpa * (formula[i++] / 100))
         .reduce((acc, value) => acc + value, 0);
 };
-
+const showValidateMsg = () => {
+    const element = document.querySelector('.validateMsg');
+    const message = document.createElement('p');
+    message.innerText = 'Enter your GPA';
+    message.classList = 'text-danger mb-1 validateMsg';
+    if (!element) {
+        resultArea.append(message);
+    }
+    if (element) {
+        setTimeout(() => {
+            document.querySelector('.validateMsg').remove();
+        }, 4000);
+    }
+};
 const validateGPA = (value) => {
     if (value >= 2 && value <= 4) {
         return Number(value);
